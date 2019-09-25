5fdb274ba04170186557a172a5a9fa3e0bf676d0
Nanguy Marc-Henry <marchenrynanguy1@gmail.com>
2019-09-25 15:37:46 +0000

    Avant qu'il dise de refaire comme le menu du site

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,54 +1,33 @@
 $(document).ready(function () {
-    $('.mega-content').hide();
     const nav = $('nav').first();
-    console.log(nav.scrollTop());
-    $(document).scroll(function (e) { 
-        if ($(this).scrollTop() > 0) {
-            console.log('ok');
-            nav.css({
-                'backgroundColor': 'rgba(0,0,0,0.5)',
-                'marginLeft': '0px',
-                'width': '100%',
-                'padding-right': '30px'
 
-            });
+    $(document).scroll(function () {
+        if (($(this).scrollTop() > 0)) {
+            nav.addClass('fixed-top');
+            nav.css('backgroundColor', 'white')
+            $('nav .btn-outline-light').first().addClass('btn-outline-dark').removeClass('btn-outline-light');
+            $('nav *').css('color', 'black')
         } else {
-            nav.css({
-                'backgroundColor': 'transparent',
-                'marginLeft': '10%',
-                'width': '80%',
-                'padding-right': '0'
-            });
+            nav.removeClass('fixed-top');
+            nav.css('backgroundColor', 'transparent')
+            $('nav .btn-outline-dark').first().addClass('btn-outline-light').removeClass('btn-outline-dark');
+            $('nav *').css('color', 'white')
         }
     });
 
-
-    $('#menu').hover(function (e) {
-        $(this).css('backgroundColor', 'gray');
-        $('.dropdown-menu').mouseleave(function (e) { 
+    $('#menu').hover(function () {
+        $(this).css('backgroundColor', 'rgb(214, 208, 208)');
+        $('.dropdown-menu').mouseleave(function (e) {
             $('#menu').css('backgroundColor', 'transparent');
-            $(this).fadeOut();   
+            $(this).fadeOut();
             e.stopPropagation();
-        }); 
-        $('.dropdown-menu').first().fadeIn();   
-         
-        $('.jlink').hover(function () { 
+        });
+        $('.dropdown-menu').first().fadeIn();
+
+        $('.jlink').hover(function () {
             $('#menu').css('backgroundColor', 'transparent');
-            $('.dropdown-menu').first().fadeOut();   
-         });// over
-         
-        }
+            $('.dropdown-menu').first().fadeOut();
+        });
 
-    );
-
- 
-
-
-    // $('.mega-link > .nav-link').hover(function (e) {
-            
-    //         console.log(e);
-    //     }, function () {
-    //         // out
-    //     }
-    // );
+    });
 });
