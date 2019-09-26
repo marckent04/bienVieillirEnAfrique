8c4af4c1261a8e2a1b8fd5cb04d94e6fe8c882fa
Nanguy Marc-Henry <marchenrynanguy1@gmail.com>
2019-09-26 18:57:18 +0000

    avant mise a jour Alexis

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,33 +1,45 @@
 $(document).ready(function () {
-    const nav = $('nav').first();
+    const nav = $('nav').first()
+
+    const createDropdown = function (selector) { 
+        $(selector).first().hover(function () {
+            $(this).parent().css({
+                'backgroundColor': 'rgb(214, 208, 208)',
+            })
+        }, function () {
+            $(this).parent().css('backgroundColor', 'transparent')     
+        }
+    );
+     } 
 
     $(document).scroll(function () {
         if (($(this).scrollTop() > 0)) {
-            nav.addClass('fixed-top');
+            nav.addClass('fixed-top')
             nav.css('backgroundColor', 'white')
-            $('nav .btn-outline-light').first().addClass('btn-outline-dark').removeClass('btn-outline-light');
+            $('nav .btn-outline-light').first().addClass('btn-outline-dark').removeClass('btn-outline-light')
             $('nav *').css('color', 'black')
         } else {
             nav.removeClass('fixed-top');
             nav.css('backgroundColor', 'transparent')
-            $('nav .btn-outline-dark').first().addClass('btn-outline-light').removeClass('btn-outline-dark');
+            $('nav .btn-outline-dark').first().addClass('btn-outline-light').removeClass('btn-outline-dark')
             $('nav *').css('color', 'white')
         }
-    });
+    })
 
-    $('#menu').hover(function () {
-        $(this).css('backgroundColor', 'rgb(214, 208, 208)');
-        $('.dropdown-menu').mouseleave(function (e) {
-            $('#menu').css('backgroundColor', 'transparent');
-            $(this).fadeOut();
-            e.stopPropagation();
-        });
-        $('.dropdown-menu').first().fadeIn();
 
-        $('.jlink').hover(function () {
-            $('#menu').css('backgroundColor', 'transparent');
-            $('.dropdown-menu').first().fadeOut();
-        });
+    createDropdown('div.agisDrop')
 
-    });
-});
+    createDropdown('div.actionsDrop')
+
+    createDropdown('div.we')
+
+    createDropdown('div.clickable')
+
+    // $('li.clickable').click(function () { 
+    //     alert($(this).css('backgroundColor'))
+    //     if ($(this).css('backgroundColor') == 'rgb(0, 0, 0)')
+    //         $(this).css('backgroundColor', 'rgb(214, 208, 208)');
+    //     else
+    //         $(this).css('backgroundColor', 'transparent');
+    // });
+})
